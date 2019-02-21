012e8824f8c1cb53d69bcd7ecee2e967c7f1e0a9
shannon-nabors <shannon.nabors@gmail.com>
2019-02-20 19:41:57 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-function handleSubmit(e) {
-
-}
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
+
+function handleSubmit(e) {
+  e.preventDefault()
+  resetIdenticon()
+  let input = document.querySelector('input').value
+  console.log(input)
+  let id
+  if (input != "") {
+    id = new Identicon(input)
+  updateDOM(id)
+  } else {
+    resetIdenticon()
+  }
+  this.reset()
+}
