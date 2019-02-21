cdee1350df829d11cac03c9cef8ec2845d00828b
mkay0961 <mkay0961@gmail.com>
2019-02-21 12:11:33 -0500

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,39 @@
-function updateDOM() {
+function updateDOM(emoji) {
+    let color = emoji.colorCreate()
+    let shaded = emoji.shadedArray
+    let grid = document.getElementById("identicon").children
+    let fullarray = emoji.createFull()
+    for(let i = 1 ; i <= fullarray.length; i++){
+      setGridElement(fullarray[i-1],getGridElement(i), color)
+    }
+
+
+}
+
+function getGridElement(position){
+  let count = 1
+  for (let i = 0; i <= 4; i++) {
+    for (let k = 0; k <= 4; k++) {
+      if(position == count){
+        return `${k}-${i}`
+      }
+      count +=1
+
+    }
+  }
+
+
+
+}
+
+function setGridElement(shaded, position, color){
+  let el = document.getElementById(position)
+  if(shaded){
+    el.style.backgroundColor = `${color}`
+  }else{
+    el.style.backgroundColor = "white"
+  }
+  // debugger
+
 
 }
--- src/identicon.js
+++ src/identicon.js
@@ -1,3 +1,33 @@
 class Identicon {
   // make use of the md5.array method, which is in scope because we required it in our index.html
+  constructor(str) {
+    this.str = str;
+    this.hash = md5.array(str);
+    this.color = this.hash
+
+    let newArray = []
+    this.hash.forEach((x) => {newArray.push(shadeOrNot(x))})
+    this.shadedArray = newArray
+
+  }
+
+  colorCreate(){
+      return `rgb(${this.color[0]},${this.color[1]},${this.color[2]})`
+  }
+
+  createFull(){
+    let newArray = []
+    let firstHalf = this.shadedArray.slice(0,10)
+    let middle = this.shadedArray.slice(10,15)
+    let lastHalf = firstHalf.slice(5,10).concat(firstHalf.slice(0,5))
+    newArray = firstHalf.concat(middle);
+    newArray = newArray.concat(lastHalf);
+
+    return newArray
+
+  }
+
+}
+function shadeOrNot(num){
+  return (num %2 == 0)
 }
