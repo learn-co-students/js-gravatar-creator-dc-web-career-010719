5bbd8622e3e8555f25cde3f7165164cf624150fb
Ben Yellin <byellindc@gmail.com>
2019-02-21 12:32:48 -0500

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,13 @@
 function updateDOM() {
 
 }
+
+function renderIdenticon(identicon) {
+  const container = document.getElementById('identicon');
+  container.innerHTML = '';
+
+  identicon.hashList.forEach(h => {
+    const block = document.createElement('span')
+    block
+  });
+}
--- src/identicon.js
+++ src/identicon.js
@@ -1,3 +1,8 @@
 class Identicon {
   // make use of the md5.array method, which is in scope because we required it in our index.html
+
+  constructor(name) {
+    this.name = name;
+    this.hashList = md5.array(name);
+  }
 }
